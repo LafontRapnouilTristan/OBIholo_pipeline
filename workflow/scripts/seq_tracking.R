5f3d44ee5d31362c956cb9886c804b6c79334dec
Tristan Lafont Rapnouil <you@example.com>
2023-01-10 16:17:22 -0300

    correct indexing of snakemake@inputs and outputs

--- workflow/scripts/seq_tracking.R
+++ workflow/scripts/seq_tracking.R
@@ -4,86 +4,78 @@ sink(log)
 sink(log, type="message")
 
 path <- snakemake@input
+params <- snakemake@params 
+outs <- snakemake@output
+
+# Global ####
 
 # seq count
-cmd <- paste0("grep -c '@' " ,path[[1]])
-ali <- system(cmd,intern = T)
-
-cmd <- paste0("grep -c '@' " ,path[[2]])
-qualali <- system(cmd,intern = T)
-
-cmd <- paste0("grep -c '@' " ,path[[3]])
-dml <- system(cmd,intern = T)
-
-cmd <- paste0("grep -R '@' ",path[[4]]," | wc -l " )
-filtrim <- system(cmd,intern = T)
-
-cmd <- paste0("grep -c '>' " ,path[[5]])
-derep <- system(cmd,intern = T)
-
-cmd <- paste0("grep -c '>' " ,path[[6]])
-clean <- system(cmd,intern = T)
-
-cmd <- paste0("grep -c '>' " ,path[[7]])
-abbfilt <- system(cmd,intern = T)
-
-cmd <- paste0("grep -c '>' " ,path[[8]])
-bimera <- system(cmd,intern = T)
-
-cmd <- paste0("grep -c '>' " ,path[[9]])
-clust <- system(cmd,intern = T)
-
-cmd <- paste0("grep -c '>' " ,path[[10]])
-agg <- system(cmd,intern = T)
+#fastq
+cmd <- paste0("grep -c '@' " ,path[1:2])
+ali <- system(cmd[1],intern = T)
+qualali <- system(cmd[2],intern = T)
+#fasta
+cmd <- paste0("grep -c '>' " ,path[3:8])
+dml <- system(cmd[1],intern = T)
+basicfilt <- system(cmd[2],intern = T)
+derep <- system(cmd[3],intern = T)
+clean <- system(cmd[4],intern = T)
+clust <- system(cmd[5],intern = T)
+agg <- system(cmd[6],intern = T)
 
 # reads count
-cmd <- paste0("grep -o -P '(?<=\\scount=)[0-9]+' ",path[[5]]," | awk -F: '{n+=$1} END {print n}'" )
-derep_reads <- system(cmd,intern = T)
-
-cmd <- paste0("grep -o -P '(?<=\\scount=)[0-9]+' ",path[[6]]," | awk -F: '{n+=$1} END {print n}'" )
-clean_reads <- system(cmd,intern = T)
-
-cmd <- paste0("grep -o -P '(?<=\\scount=)[0-9]+' ",path[[7]]," | awk -F: '{n+=$1} END {print n}'" )
-abbfilt_reads <- system(cmd,intern = T)
-
-cmd <- paste0("grep -o -P '(?<=\\scount=)[0-9]+' ",path[[8]]," | awk -F: '{n+=$1} END {print n}'" )
-bimera_reads <- system(cmd,intern = T)
-
-cmd <- paste0("grep -o -P '(?<=\\scount=)[0-9]+' ",path[[9]]," | awk -F: '{n+=$1} END {print n}'" )
-clust_reads <- system(cmd,intern = T)
-
-cmd <- paste0("grep -o -P '(?<=\\scount=)[0-9]+' ",path[[10]]," | awk -F: '{n+=$1} END {print n}'" )
-agg_reads <- system(cmd,intern = T)
+cmd <- paste0("grep -o -P '(?<=\\scount=)[0-9]+' ",path[5:8]," | awk -F: '{n+=$1} END {print n}'" )
+derep_reads <- system(cmd[1],intern = T)
+clean_reads <- system(cmd[2],intern = T)
+clust_reads <- system(cmd[3],intern = T)
+agg_reads <- system(cmd[4],intern = T)
 
 df <- data.frame(step=c("aligned",
                         "alignement qality filtering",
                         "demultiplexing",
-                        "filter and trim",
+                        "basic filtration",
                         "dereplication",
                         "obiclean",
-                        "abbundance filtering",
-                        "bimera removal",
                         "clustering",
                         "merging cluster"),
                  total_sequence=c(ali,
                                   qualali,
                                   dml,
-                                  filtrim,
+                                  basicfilt,
                                   derep,
                                   clean,
-                                  abbfilt,
-                                  bimera,
                                   clust,
                                   agg),
                  reads=c(ali,
                          qualali,
                          dml,
-                         filtrim,
+                         basicfilt,
                          derep_reads,
                          clean_reads,
-                         abbfilt_reads,
-                         bimera_reads,
                          clust_reads,
                          agg_reads)
 )
-write.csv2(df,snakemake@output[[1]],row.names=F)
+write.table(df,file=snakemake@output[[1]],row.names=F,sep="\t",quote=F)
+
+# Sample wise ####
+
+# sample wise derepled
+
+cmd2 <- paste0("bash workflow/scripts/reads_count.sh ", path[5:8], " derepl ", params)
+system(cmd2[1],intern = T)
+system(cmd2[2],intern = T)
+system(cmd2[3],intern = T)
+system(cmd2[4],intern = T)
+# sample wise dml and basfilt
+
+#reads
+cmd3 <- paste0("grep -o -P 'sample_ID=.{1,10};' ", path[3:4],"| sort | uniq -c | sed 's/sample_ID=//g' | sed 's/;//g' > ", outs[c(3,5)])
+system(cmd3[1],intern = T)
+system(cmd3[2],intern = T)
+#uniqseq 
+cmd4 <- paste0("bash workflow/scripts/count_dml ", path[3:4], " ", outs[c(2,4)] )
+system(cmd4[1],intern = T)
+system(cmd4[2],intern = T)
+
+
+
