a045d7abc46a542732061a5535a8627e107c9664
Tristan Lafont Rapnouil <you@example.com>
2023-01-12 16:14:48 -0300

    add bootstrap score to dada2 tax output

--- workflow/scripts/taxassign_dada2.R
+++ workflow/scripts/taxassign_dada2.R
@@ -14,7 +14,8 @@ tab <- data.frame(seq_id = str_extract(names(cleaned_seq),"seq[0-9]+"),
 tabseq<- makeSequenceTable(tab)
 taxa <- assignTaxonomy(tabseq,
                        snakemake@params[[2]],
-                       multithread=snakemake@params[[1]])
+                       multithread=snakemake@params[[1]],
+                       outputBootstraps = T)
 
 taxa <-cbind(sequences=rownames(taxa),taxa)
 
